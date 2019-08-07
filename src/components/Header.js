43f0cd445c902a9f88597dc30bc8e53fec6af463
Todd Ivey <toddivey@gmail.com>
2019-08-07 17:08:27 -0500

    added dynamic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,20 @@ import { withRouter } from "react-router-dom";
 
 class Header extends Component {
   render() {
-    const path = this.props.location.pathname
-    if (path === '/'){
+    let path = this.props.location.pathname
+      if (path === '/'){
+        return (
+          <h1 className='App-header'>Welcome</h1>
+        );
+      }
+    path = (path.slice(1))
+    path = path.charAt(0).toUpperCase() + path.slice(1)
       return (
-        <h1 className='App-header'>Welcome</h1>
-    );
-    }
-    return (
-      <h1 className='App-header'>
-        <img src='/logo.png' style={{width: '60px', height: '50px' }} alt="Header IMG" />
-      </h1>
-  )
+        <h1 className='App-header'>
+            <img src='/logo.png' style={{width: '60px', height: '50px' }} alt="Header IMG" />
+          {path}
+        </h1>
+      )
   }
 }
 

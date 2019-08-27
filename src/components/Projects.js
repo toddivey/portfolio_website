89eb62b526914179beceb7e98c66c06a3c61c83b
Todd Ivey <toddivey@gmail.com>
2019-08-27 09:44:50 -0500

    updated resume and patched vulnerabilities

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,13 +8,13 @@ export class Projects extends Component {
           <h4>
             <a href='http://www.popnspots.com' target='_blank' rel='noopener noreferrer'>Pop N Spots
             </a></h4>
-          <p> Webapp built to quickly view the current busyness of nearby places </p>
+          <p> Webapp built to quickly view the current busyness of nearby places in Chicago. </p>
           <br />
 
           <h4>
           <a href="https://gracehopper-abmt.herokuapp.com/" target="_blank" rel="noopener noreferrer">
             graceHOPer</a> </h4>
-          <p> E-commerce website displaying thousands of products with filterable categories</p>
+          <p> E-commerce website displaying thousands of products with filterable categories.</p>
         </div>
       </div>
     )

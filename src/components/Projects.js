6110a696bb852b646513507331ceadb3f55e9e46
Todd Ivey <toddivey@gmail.com>
2019-09-09 12:25:18 -0500

    images added to projects and resume updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,34 @@ export class Projects extends Component {
       <div>
         <div className='App-body'>
           <h4>
-            <a href='http://www.popnspots.com' target='_blank' rel='noopener noreferrer'>Pop N Spots
-            </a></h4>
+            <div>
+              <div id="title">
+            <a href='http://www.popnspots.com' target='_blank' rel='noopener noreferrer'>Pop N Spots </a></div>
+            <div id="image">
+            <a href='http://www.popnspots.com' target='_blank' rel='noopener noreferrer'>
+            <img src='/images/Screen Shot-popNspots.png' style={{height: '50%', width: '30%', padding:'2%'}} alt="popNspots screenshot" />
+            </a>
+            </div>
+            </div>
+          </h4>
           <p> Webapp built to quickly view the current busyness of nearby places in Chicago. </p>
+
           <br />
 
           <h4>
-          <a href="https://gracehopper-abmt.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            graceHOPer</a> </h4>
+          <div>
+              <div id="title">
+            <a href="https://gracehopper-abmt.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              graceHOPer
+              </a>
+              </div>
+              <div id="image">
+              <a href="https://gracehopper-abmt.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <img src='/images/Screen Shot-graceHOPper.png' style={{height: '50%', width: '30%', padding:'2%'}} alt="graceHOPper screen shot" />
+            </a>
+            </div>
+            </div>
+          </h4>
           <p> E-commerce website displaying thousands of products with filterable categories.</p>
         </div>
       </div>
